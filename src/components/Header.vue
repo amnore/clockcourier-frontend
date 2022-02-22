61109b1181b8736c7ee1fa89e9b2ec87e56a97c6
cn-zheshi <1760864645@qq.com>
2022-02-23 01:52:36 +0800

    导航栏

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <table class="head">
+            <tr>
+                <td><router-link to="/repositories">仓库</router-link></td>
+                <td><router-link to="/projects">项目</router-link></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "header",
+  props: {},
+}
+</script>、
+
+<style>
+.head{
+    text-align: center;
+    margin: auto;
+}
+
+</style>
