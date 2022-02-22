e5d88ac09029d4dcc11ac53bd28da88438b8262d
cn-zheshi <1760864645@qq.com>
2022-02-23 01:54:18 +0800

    去除不必要的log

--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from './scripts/router'
 
-const app=createApp(App).use(router).mount("#app")
-console.log(app.$router.getRoutes());
+createApp(App).use(router).mount("#app")
