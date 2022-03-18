077343a475a731c41603288b735d1a032343948f
cn-zheshi <1760864645@qq.com>
2022-03-18 15:32:35 +0800

    style:将Page组件应用于project和repository页面

--- src/views/project.vue
+++ src/views/project.vue
@@ -133,49 +133,7 @@
             <el-table-column prop="dependencyType" label="依赖类型" />
           </el-table>
         </div>
-        <div>
-          <ul class="page">
-            <li>
-              <span v-if="page > 1"
-                ><b @click="page--, getDependency(page)">上一页</b></span
-              >
-              <span v-if="page == 1">上一页</span>
-              <span v-if="Number(pageAll) <= 10">
-                <span
-                  v-for="index in pageAll"
-                  :key="index"
-                  :class="{ active: page == index }"
-                  @click="goPage(index)"
-                  >{{ index }}</span
-                ></span
-              >
-              <span v-if="Number(pageAll) > 10">
-                <span @click="goPage(1)">1</span>
-                <span v-if="page > 3">...</span>
-                <span v-if="page > 2" @click="goPage(page - 1)">{{
-                  page - 1
-                }}</span>
-                <span v-if="page > 1 && page < pageAll" @click="goPage(page)">{{
-                  page
-                }}</span>
-                <span v-if="page < pageAll - 1" @click="goPage(page + 1)">{{
-                  page + 1
-                }}</span>
-                <span v-if="page < pageAll - 2">...</span>
-                <span @click="goPage(pageAll)">{{ pageAll }}</span>
-              </span>
-              <span v-if="page != pageAll"
-                ><b @click="page++, getDependency(page)">下一页</b></span
-              >
-              <span v-if="page == pageAll">下一页</span>
-            </li>
-            <li>共{{ pageAll }}页 当前{{ page }}页</li>
-            <li>到</li>
-            <li><el-input type="text" value="1" v-model="jumpPage" /></li>
-            <li>页</li>
-            <el-button v-on:click="goPage(jumpPage)">确定</el-button>
-          </ul>
-        </div>
+        <page :goPage="goPage" :pageAll="pageAll"></page>
       </el-main>
     </el-container>
   </el-container>
@@ -188,10 +146,12 @@ import {
 } from "../api/search_project";
 import { get_repo_by_id } from "../api/search_repo";
 import { dateFormatter } from "@/scripts/DateFormatter.js";
+import Page from "../components/Page.vue";
 
 export default {
   name: "Project", //注册在路由（router.js）里的就是这个
   props: {},
+  components: { Page },
   data() {
     return {
       project: {
@@ -314,18 +274,5 @@ export default {
 .el-aside {
   margin-left: 3%;
   margin-top: 3%;
-}
-
-.page li {
-  display: inline-block;
-  margin: 0 5px;
-}
-.page li span {
-  display: inline-block;
-  padding: 5px 10px;
-  border: 1px solid #dfdfdf;
-  margin: 0 5px;
-  border-radius: 5px;
-  cursor: pointer;
 }
 </style>
--- src/views/repository.vue
+++ src/views/repository.vue
@@ -132,49 +132,7 @@
           <el-table-column prop="dependencyType" label="依赖类型" />
         </el-table>
 
-        <div>
-          <ul class="page">
-            <li>
-              <span v-if="page > 1"
-                ><b @click="page--, getDependency(page)">上一页</b></span
-              >
-              <span v-if="page == 1">上一页</span>
-              <span v-if="Number(pageAll) <= 10">
-                <span
-                  v-for="index in pageAll"
-                  :key="index"
-                  :class="{ active: page == index }"
-                  @click="goPage(index)"
-                  >{{ index }}</span
-                ></span
-              >
-              <span v-if="Number(pageAll) > 10">
-                <span @click="goPage(1)">1</span>
-                <span v-if="page > 3">...</span>
-                <span v-if="page > 2" @click="goPage(page - 1)">{{
-                  page - 1
-                }}</span>
-                <span v-if="page > 1 && page < pageAll" @click="goPage(page)">{{
-                  page
-                }}</span>
-                <span v-if="page < pageAll - 1" @click="goPage(page + 1)">{{
-                  page + 1
-                }}</span>
-                <span v-if="page < pageAll - 2">...</span>
-                <span @click="goPage(pageAll)">{{ pageAll }}</span>
-              </span>
-              <span v-if="page != pageAll"
-                ><b @click="page++, getDependency(page)">下一页</b></span
-              >
-              <span v-if="page == pageAll">下一页</span>
-            </li>
-            <li>共{{ pageAll }}页 当前{{ page }}页</li>
-            <li>到</li>
-            <li><el-input type="text" value="1" v-model="jumpPage" /></li>
-            <li>页</li>
-            <el-button v-on:click="goPage(jumpPage)">确定</el-button>
-          </ul>
-        </div>
+        <page :goPage="goPage" :pageAll="pageAll"></page>
       </el-main>
     </el-container>
   </el-container>
@@ -182,8 +140,10 @@
 <script>
 import { get_repo_by_id, get_repo_dependency } from "../api/search_repo";
 import { dateFormatter } from "@/scripts/DateFormatter.js";
+import Page from "../components/Page.vue";
 export default {
   name: "Repository",
+  components: { Page },
   props: {},
   data() {
     return {
@@ -290,5 +250,4 @@ export default {
   margin-left: 3%;
   margin-top: 3%;
 }
-
 </style>
