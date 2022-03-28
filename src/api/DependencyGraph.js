6f2382445e55ca01b17d87536b8cdee169eae3c1
Chen Zhenge <191250013@smail.nju.edu.cn>
2022-03-28 15:04:48 +0800

    depgraph: use cached result

--- src/api/DependencyGraph.js
+++ src/api/DependencyGraph.js
@@ -1,7 +1,13 @@
 import { backendUrl } from './Url'
 
-export default function getGraph(libId) {
+export function getGraph(libId) {
   return fetch(`${backendUrl}/migration/mvn/lib/${libId}/get`, {
+    headers: { 'Content-Type': 'application/json' },
+  }).then(resp => resp.json())
+}
+
+export function getDependencyInfo(libId) {
+  return fetch(`${backendUrl}/project/mvn/lib/get/${libId}`, {
     headers: { 'Content-Type': 'application/json' },
   }).then(resp => resp.json())
 }
